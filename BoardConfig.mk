29f9bd8705d527af4e0af1d970a02c118bd06688
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-14 21:26:39 +0800

    nx591j: Start device using common tree

---
+++ BoardConfig.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common msm8976-common
+-include device/nubia/msm8976-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/nubia/nx591j
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_nx591j_defconfig
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4831838208
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 56428567552
+
+# Sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# inherit from the proprietary version
+-include vendor/nubia/nx591j/BoardConfigVendor.mk
